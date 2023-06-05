bdb725dc50e60811b3f6c3533235703284975b42
jamie.liu <jamie.liu@ucloud.cn>
2023-06-05 22:26:14 +0800

    insertion sort

---
+++ sort/insertionSort.js
@@ -0,0 +1,17 @@
+function insertionSort(arr) {
+  let len = arr.length;
+  if (arr.length <= 1) return arr;
+  for (let i = 1; i <= len - 1; i++) {
+    let current = arr[i]; // 待插入的值
+    // 从后往前遍历已排序区间
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j] > current) {
+        arr[j + 1] = arr[j]; // 往后移
+      } else {
+        arr[j + 1] = current; // 插入并结束遍历
+        break;
+      }
+    }
+  }
+  return arr;
+}
